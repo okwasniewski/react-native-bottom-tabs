638dd2dfff6b1fc03a23cac387912298e12b2371
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-10-07 23:00:54 +0200

    add ios/generated to gitignore

--- android/src/oldarch/RCTTabViewViewManager.kt
+++
@@ -1,9 +0,0 @@
-package com.rcttabview
-
-import android.view.View
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.uimanager.SimpleViewManager
-
-abstract class RCTTabViewViewManagerSpec<T : View> : SimpleViewManager<T>() {
-  abstract fun setColor(view: T?, value: String?)
-}
