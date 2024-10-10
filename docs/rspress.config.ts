0c8680faea8cd858f8f8b6105c59eb8afbc6a909
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-10-10 14:41:49 +0200

    feat: base docs

---
+++ docs/rspress.config.ts
@@ -0,0 +1,17 @@
+import * as path from 'path';
+import { defineConfig } from 'rspress/config';
+
+export default defineConfig({
+  root: path.join(__dirname, 'docs'),
+  title: 'Bottom Tabs',
+  description: 'React Native Bottom Tabs Documentation',
+  themeConfig: {
+    socialLinks: [
+      {
+        icon: 'github',
+        mode: 'link',
+        content: 'https://github.com/web-infra-dev/rspress',
+      },
+    ],
+  },
+});
