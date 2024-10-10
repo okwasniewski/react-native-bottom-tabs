25ca4b431d88402ec0d49ca0519bf5fd96c04460
Mike Hamilton <mike@mikehamilton.ca>
2024-10-10 20:18:49 +0200

    refactor: update with proper types working with 0.0.8

--- src/expo-router/index.ts
+++ src/expo-router/index.ts
@@ -6,21 +6,14 @@ import {
   NativeBottomTabNavigationOptions,
 } from 'react-native-bottom-tabs/react-navigation';
 
-// This should be imported from react-native-bottom-tabs/react-navigation which is
-// exporting NativeBottomTabNavigationOptions but the types seem to be broken at
-// at the moment..
-// import { BottomTabNavigationOptions } from '@react-navigation/bottom-tabs';
-
 import type {
   ParamListBase,
   TabNavigationState,
 } from '@react-navigation/native';
 
-const { Navigator } = createNativeBottomTabNavigator();
-
 export const NativeTabs = withLayoutContext<
   NativeBottomTabNavigationOptions,
-  typeof Navigator,
+  ReturnType<typeof createNativeBottomTabNavigator>['Navigator'],
   TabNavigationState<ParamListBase>,
   NativeBottomTabNavigationEventMap
->(Navigator);
+>(createNativeBottomTabNavigator().Navigator);
