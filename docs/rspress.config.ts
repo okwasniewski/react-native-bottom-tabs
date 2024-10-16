22fe3311525110267dd97e127aca78c99355d6ed
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-10-16 19:22:29 +0200

    docs: add phone logo

--- docs/rspress.config.ts
+++ docs/rspress.config.ts
@@ -1,11 +1,15 @@
 import * as path from 'path';
 import { defineConfig } from 'rspress/config';
+import { pluginOpenGraph } from 'rsbuild-plugin-open-graph';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
   base: '/react-native-bottom-tabs/',
-  title: 'Native Bottom Tabs',
+  title: 'React Native Bottom Tabs',
   description: 'React Native Bottom Tabs Documentation',
+  logoText: 'React Native Bottom Tabs',
+  icon: '/img/phone.png',
+  logo: '/img/phone.png',
   themeConfig: {
     socialLinks: [
       {
@@ -13,6 +17,16 @@ export default defineConfig({
         mode: 'link',
         content: 'https://github.com/okwasniewski/react-native-bottom-tabs',
       },
+    ],
+  },
+  builderConfig: {
+    plugins: [
+      pluginOpenGraph({
+        title: 'React Native Bottom Tabs',
+        type: 'website',
+        url: 'https://okwasniewski.github.io/react-native-bottom-tabs/',
+        description: 'Native Bottom Tabs for React Native',
+      }),
     ],
   },
 });
