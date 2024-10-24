7d725d18ef8a1b15b22e86b8538b2a8735975e37
shubhamguptadream11 <shubham.gupta@dream11.com>
2024-10-24 11:07:23 +0530

    fix: index handling

--- src/react-navigation/views/NativeBottomTabView.tsx
+++ src/react-navigation/views/NativeBottomTabView.tsx
@@ -51,10 +51,15 @@ export default function NativeBottomTabView({
         return null;
       }}
       getLazy={({ route }) => descriptors[route.key]?.options.lazy ?? true}
-      onTabLongPress={(key) => {
+      onTabLongPress={(index) => {
+        const route = state.routes[index];
+        if (!route) {
+          return;
+        }
+
         navigation.emit({
           type: 'tabLongPress',
-          target: key,
+          target: route.key,
         });
       }}
       onIndexChange={(index) => {
