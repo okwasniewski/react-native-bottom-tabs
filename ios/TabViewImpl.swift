535557c5fbb8bf802f77eb4de7e1aefe4dc8c968
Douglas Lowder <douglowder@mac.com>
2024-10-27 12:09:34 -0700

    feat: add Apple TV support

--- ios/TabViewImpl.swift
+++ ios/TabViewImpl.swift
@@ -188,9 +188,11 @@ extension View {
   
   @ViewBuilder
   func tabBadge(_ data: String?) -> some View {
-    if #available(iOS 15.0, macOS 15.0, visionOS 2.0, *) {
+    if #available(iOS 15.0, macOS 15.0, visionOS 2.0, tvOS 15.0, *) {
       if let data = data, !data.isEmpty {
+        #if !os(tvOS)
         self.badge(data)
+        #endif
       } else {
         self
       }
