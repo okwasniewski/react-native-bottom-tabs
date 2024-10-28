b61de371de71004a5aa1da3855d2af4127aa875e
shubhamguptadream11 <shubham.gupta@dream11.com>
2024-10-28 15:45:19 +0530

    fix: removed color fro native bottom tabs

--- example/src/Examples/NativeBottomTabs.tsx
+++ example/src/Examples/NativeBottomTabs.tsx
@@ -13,7 +13,6 @@ function NativeBottomTabs() {
     <Tab.Navigator
       tabBarInactiveTintColor="#C57B57"
       tabBarActiveTintColor="#F7DBA7"
-      activeIndicatorColor={'yellow'}
       barTintColor="#1E2D2F"
       rippleColor="#041F1E"
       screenListeners={{
