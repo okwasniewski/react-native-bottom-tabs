55b4e45c014bdc7c16d1fc2c9cf5a520181706fa
Oskar Kwaśniewski <oskarkwasniewski@icloud.com>
2024-11-06 09:08:17 +0100

    feat: add embedded stack example

---
+++ example/src/Examples/NativeBottomTabsEmbeddedStacks.tsx
@@ -0,0 +1,104 @@
+import { Article } from '../Screens/Article';
+import { Albums } from '../Screens/Albums';
+import { Contacts } from '../Screens/Contacts';
+import { Chat } from '../Screens/Chat';
+import createNativeBottomTabNavigator from '../../../src/react-navigation/navigators/createNativeBottomTabNavigator';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const headerOptions = {
+  headerShown: true,
+  headerLargeTitle: true,
+};
+
+const Tab = createNativeBottomTabNavigator();
+
+const ArticleStack = createNativeStackNavigator();
+const AlbumsStack = createNativeStackNavigator();
+const ContactsStack = createNativeStackNavigator();
+const ChatStack = createNativeStackNavigator();
+
+function ArticleStackScreen() {
+  return (
+    <ArticleStack.Navigator>
+      <ArticleStack.Screen
+        options={{ ...headerOptions }}
+        name="ArticleScreen"
+        component={Article}
+      />
+    </ArticleStack.Navigator>
+  );
+}
+
+function AlbumsStackScreen() {
+  return (
+    <AlbumsStack.Navigator>
+      <AlbumsStack.Screen
+        options={{ ...headerOptions }}
+        name="AlbumsScreen"
+        component={Albums}
+      />
+    </AlbumsStack.Navigator>
+  );
+}
+
+function ContactsStackScreen() {
+  return (
+    <ContactsStack.Navigator>
+      <ContactsStack.Screen
+        options={{ ...headerOptions }}
+        name="ContactsScreen"
+        component={Contacts}
+      />
+    </ContactsStack.Navigator>
+  );
+}
+
+function ChatStackScreen() {
+  return (
+    <ChatStack.Navigator>
+      <ChatStack.Screen
+        options={{ ...headerOptions }}
+        name="ChatScreen"
+        component={Chat}
+      />
+    </ChatStack.Navigator>
+  );
+}
+
+function NativeBottomTabsEmbeddedStacks() {
+  return (
+    <Tab.Navigator ignoresTopSafeArea>
+      <Tab.Screen
+        name="Article"
+        component={ArticleStackScreen}
+        options={{
+          tabBarBadge: '10',
+          tabBarIcon: () => require('../../assets/icons/article_dark.png'),
+        }}
+      />
+      <Tab.Screen
+        name="Albums"
+        component={AlbumsStackScreen}
+        options={{
+          tabBarIcon: () => require('../../assets/icons/grid_dark.png'),
+        }}
+      />
+      <Tab.Screen
+        name="Contacts"
+        component={ContactsStackScreen}
+        options={{
+          tabBarIcon: () => require('../../assets/icons/person_dark.png'),
+        }}
+      />
+      <Tab.Screen
+        name="Chat"
+        component={ChatStackScreen}
+        options={{
+          tabBarIcon: () => require('../../assets/icons/chat_dark.png'),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+export default NativeBottomTabsEmbeddedStacks;
